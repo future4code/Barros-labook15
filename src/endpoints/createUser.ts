d93f95672fc529985c3e74c83860b5422a616a20
RenatoAraujo <nartain@gmail.com>
2023-02-05 19:03:56 -0300

    Aqui vai o grosso do projeto, só consegui mexer hoje, infelizmente!

---
+++ src/endpoints/createUser.ts
@@ -0,0 +1,39 @@
+import { connection } from "../dataBase/connection";
+import { Request, Response } from "express";
+import express, { Express } from "express";
+import cors from "express";
+import { User } from "../class/User";
+
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    let message = "Success!";
+    const { name, email, password } = req.body;
+
+    const newUser = new User(name, email, password);
+
+    if (!name || !email || !password) {
+      res.statusCode = 406;
+      message = '"name", "email" and "password" must be provided';
+      throw new Error(message);
+    }
+
+    const id: string = Date.now().toString();
+
+    await connection("labook_users").insert({
+      id,
+      name: newUser.setName(),
+      email: newUser.setEmail(),
+      password: newUser.setPassword(),
+    });
+
+    res.status(201).send({ message });
+  } catch (error: any) {
+    res.statusCode = 400;
+    let message = error.sqlMessage || error.message;
+    res.send({ message });
+  }
+};
